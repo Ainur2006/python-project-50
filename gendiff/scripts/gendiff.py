0c41a27d57938425aa7437d31ba1398932b3c1b8
Айнур Халилов <xalilovajnur@gmail.com>
2026-06-04 15:46:12 +0500

    добавил линтер ruff и настроил его

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 
+
 def generate_diff(file_path1, file_path2):
     data1 = json.load(open(file_path1))
     data2 = json.load(open(file_path2))
@@ -18,7 +19,7 @@ def generate_diff(file_path1, file_path2):
 
     for key in all_keys:
         if key in data1 and key not in data2:
-            lines.append(f'  - {key}: {format_bool(data1[key])}')   # result[f'- {key}'] = data1[key]
+            lines.append(f'  - {key}: {format_bool(data1[key])}')
         elif key not in data1 and key in data2:
             lines.append(f'  + {key}: {format_bool(data2[key])}')
         elif data1[key] != data2[key]:
