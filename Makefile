d4ae7eba481f1c59bffe3cacfd661ec8e1837951
Айнур Халилов <xalilovajnur@gmail.com>
2026-06-05 11:13:03 +0500

    добавил тест, линтер, бэджик SonarQube

---
+++ Makefile
@@ -0,0 +1,11 @@
+install:
+	uv sync
+
+diff:
+	uv run gendiff tests/fixtures/file1.json tests/fixtures/file2.json
+
+check:
+	uv run ruff check .
+
+pytest:
+	uv run pytest
---
+++ tests/test_gendiff.py
@@ -0,0 +1,11 @@
+from gendiff.scripts.gendiff import generate_diff
+
+
+def test_gendiff():
+    assert generate_diff(
+        "tests/fixtures/file1.json",
+        "tests/fixtures/file2.json"
+    ) == (
+        "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n"
+        "  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}"
+    )
